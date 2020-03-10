30ffb936d038c11a1c0f0ab4baceebf88d07b455
steve choi <steve.choi@telus.com>
2020-03-09 20:19:39 -0400

    fix: update array data type

--- create_tables/create_team_hierarchy.sql
+++ create_tables/create_team_hierarchy.sql
@@ -2,7 +2,7 @@ CREATE TABLE employee_db.teams_hierarchy (
   team_id INT64 OPTIONS(description="Team ID"),
   team_level STRING OPTIONS(description="Type of team e.g. manager, director, VP"),
   team_name STRING OPTIONS(description="Team name"),
-  sub_teams ARRAY<STRING> OPTIONS(description="Array of teams that are contained within this team"),
+  sub_teams ARRAY<INT64> OPTIONS(description="Array of teams that are contained within this team"),
 )
 OPTIONS (
   description="Table containing team IDs and names with hierarchical relationships"
